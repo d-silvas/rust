c8f34035f7de28262bb79bd379ead71de58d6862
David Silva <d.silvas@outlook.com>
2024-03-14 07:25:01 +0000

    add benchmarking

--- parallel/src/main.rs
+++ parallel/src/main.rs
@@ -1,6 +1,9 @@
+use env_logger::Env;
 use is_prime::*;
+use log::debug;
 use num_bigint::BigUint;
 use rayon::prelude::*;
+use std::time::Instant;
 
 #[derive(Debug)]
 struct Candidate {
@@ -9,20 +12,34 @@ struct Candidate {
 }
 
 fn main() {
-    let mut candidates: Vec<Candidate> = Vec::with_capacity(1000);
+    env_logger::Builder::from_env(Env::default().default_filter_or("debug")).init();
+
+    let start = Instant::now();
+
+    let num_iter: u32 = 100;
+    let mut candidates: Vec<Candidate> = Vec::with_capacity(num_iter as usize);
     let large_num: BigUint =
         // BigUint::parse_bytes(b"1000000000000000000000000000000000000000", 10).unwrap();
         BigUint::parse_bytes(b"10000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000", 10).unwrap();
-    for i in 0_u32..=999_u32 {
+    debug!("Create vars: {:.2?}", start.elapsed());
+
+    let before = Instant::now();
+    for i in 0_u32..num_iter {
         let next: BigUint = &large_num + &BigUint::from(i);
         candidates.push(Candidate {
             num: next,
             is_prime: false,
         })
     }
+    debug!("Create cand: {:.2?}", before.elapsed());
+
+    let before = Instant::now();
     candidates.par_iter_mut().for_each(|c: &mut Candidate| {
         c.is_prime = is_prime(c.num.to_string().as_str());
     });
+    debug!("Prim checks: {:.2?}", before.elapsed());
 
-    println!("{:#?}", candidates);
+    debug!("TOTAL: {:.2?}", start.elapsed());
+
+    // println!("{:#?}", candidates);
 }
